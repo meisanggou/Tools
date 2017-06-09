04fc67f891d7d4e09bda346708bfcd0799b5c330
meisanggou <zhou5315938@163.com>
2017-06-09 15:03:15 +0800

    0.1.20

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info <= (2, 7):
     sys.exit(1)
 
 name = "JYTools"
-version = "0.1.19"
+version = "0.1.20"
 url = "https://github.com/meisanggou/Tools"
 license = "MIT"
 author = "meisanggou"
@@ -30,7 +30,7 @@ setup(name=name,
       author=author,
       author_email="zhouheng@gene.ac",
       url=url,
-      packages=["JYTools"],
+      packages=["JYTools", "JYTools/JYWorker"],
       license=license,
       description=short_description,
       long_description=long_description,
