742aba358bed3bbb6ada1c2e37f1852c002e9177
meisanggou <zhou5315938@163.com>
2017-06-12 15:45:08 +0800

    add receive_argv

--- JYTools/JYWorker/__init__.py
+++ JYTools/JYWorker/__init__.py
@@ -1,7 +1,21 @@
 #! /usr/bin/env python
 # coding: utf-8
 
+import os
 from _redis import RedisWorker, RedisQueue
 from _Task import TaskStatus
 
 __author__ = 'meisanggou'
+
+
+def receive_argv(d, short_opt, long_opt, default_value=None):
+    short_opt_key = "-" + short_opt
+    long_opt_key = "--" + long_opt
+    if short_opt_key in d:
+        return d[short_opt_key]
+    if long_opt_key in d:
+        return d[long_opt_key]
+    default_value = os.environ.get(long_opt.replace("-", "_").upper(), default_value)
+    if default_value is None:
+        exit("please use %s or %s" % (short_opt_key, long_opt_key))
+    return default_value
