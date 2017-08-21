fd1d9770d62c55bfdd0f6db76d0ef66b5029a099
meisanggou <zhou5315938@163.com>
2017-08-21 14:43:16 +0800

    fix 0.2.5

--- JYTools/JYWorker/_du.py
+++ JYTools/JYWorker/_du.py
@@ -159,7 +159,7 @@ class DAGWorker(RedisWorker):
             self.del_task_item(index)
 
     def analysis_ref(self, ref_str, current_index, task_len):
-        match_r = re.match("^(\\d{1,10})([a-z]\\w{0,20})$", ref_str)
+        match_r = re.match("^(\\d{1,10})([a-z]\\w{0,20})$", ref_str, re.I)
         if match_r is None:
             return False, "Input Not Standard Ref Result Format %s" % ref_str
         ref_index = int(match_r.groups()[0])
