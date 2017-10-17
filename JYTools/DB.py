9787810c95ef1e2715cf39e928bac7360c370e84
meisanggou <zhou5315938@163.com>
2017-10-17 16:47:03 +0800

    cat set host 0.3.8

--- JYTools/DB.py
+++ JYTools/DB.py
@@ -342,11 +342,11 @@ class DB(object):
                 g_sql = "GRANT SELECT ON {db}.* TO %s@%s;"
             self.execute(g_sql.format(db=db), args=(user, host))
 
-    def root_init_conf(self):
+    def root_init_conf(self, host='localhost'):
         o = self._read_conf(self.conf_path, False)
-        self.create_user(o["db_user"], o["db_password"], db=o["db_name"], readonly=False)
+        self.create_user(o["db_user"], o["db_password"], host=host, db=o["db_name"], readonly=False)
         o = self._read_conf(self.conf_path, True)
-        self.create_user(o["db_user"], o["db_password"], db=o["db_name"], readonly=True)
+        self.create_user(o["db_user"], o["db_password"], host=host, db=o["db_name"], readonly=True)
 
     def source_file(self, file_path):
         cmd = "mysql -u%s -p%s %s < %s" % (self._db_user, self._db_password, self._db_name, file_path)
