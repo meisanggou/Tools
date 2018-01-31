c81bf8c5594937b8b81ecb21ff9c20d83ef65f51
chenyawei521 <1327158379@qq.com>
2018-01-31 17:06:30 +0800

    pipelineworker

--- JYTools/demo/asyncWorker/pipelineWorker.py
+++ JYTools/demo/asyncWorker/pipelineWorker.py
@@ -8,5 +8,5 @@ __author__ = 'meisanggou'
 
 
 p_w = DAGWorker(conf_path="redis_worker.conf", heartbeat_value="FFFFFF", log_dir="/tmp", work_tag="Pipeline",
-                 stat_work_tag="STAT")
+                stat_work_tag="STAT")
 p_w.work()
