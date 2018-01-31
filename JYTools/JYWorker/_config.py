1b1ffcf9a100cb153f4e2ccde54c1100ffd2d90c
chenyawei521 <1327158379@qq.com>
2018-01-31 16:59:27 +0800

    _config  configparser

--- JYTools/JYWorker/_config.py
+++ JYTools/JYWorker/_config.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 # coding: utf-8
-
 import os
+import six
 import tempfile
-import ConfigParser
+import configparser
 from ._Task import WorkerTask
 from redis import Redis
 
@@ -34,7 +34,7 @@ class WorkerConfig(object):
             self.work_tag = work_tag
         else:
             self.work_tag = self.DEFAULT_WORK_TAG
-        if isinstance(self.work_tag, (unicode, str)) is False and is_queue is False:
+        if isinstance(self.work_tag, (six.text_type, six.binary_type)) is False and is_queue is False:
             class_name = self.__class__.__name__
             msg = "Need String work_tag. Please Set {0}.DEFAULT_WORK_TAG=yourWorkTag Or {0}(work_tag=yourWorkTag)"
             raise TypeError(msg.format(class_name))
@@ -74,7 +74,7 @@ class WorkerConfig(object):
         return False
 
     def load_work_config(self, conf_path, section_name):
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         config.read(conf_path)
         if config.has_section(section_name):
             if config.has_option(section_name, "heartbeat_prefix_key"):
@@ -178,7 +178,7 @@ class RedisWorkerConfig(object):
         self.connected = True
 
     def load_redis_config(self, conf_path, section_name):
-        config = ConfigParser.ConfigParser()
+        config = configparser.ConfigParser()
         config.read(conf_path)
         if config.has_section(section_name):
             if config.has_option(section_name, "redis_host"):
