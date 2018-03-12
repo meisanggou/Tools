fe41e5d09797e2545adc2207a4c91fe071f0461f
meisanggou <zhou5315938@163.com>
2018-03-12 11:16:09 +0800

    update fail message

--- JYTools/JYWorker/_du.py
+++ JYTools/JYWorker/_du.py
@@ -234,7 +234,7 @@ class DAGWorker(RedisWorker):
             self.set_task_item(0, "task_status", TaskStatus.FAIL)
             self.set_task_item(0, "task_message", task_message)
             self.set_task_item(0, "task_fail_index", reporter_sub_key)
-            self.fail_pipeline("Sub Task Failed")
+            self.fail_pipeline("Sub Task ", r_task.task_sub_key, " ", r_task.work_tag, " Failed")
         if isinstance(r_task.task_output, dict):
             for output_key in r_task.task_output.keys():
                 self.set_task_item(reporter_sub_key, "output_%s" % output_key, r_task.task_output[output_key])
