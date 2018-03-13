422f5f5f8962fde37e171973bb0358890e4c8327
meisanggou <zhou5315938@163.com>
2018-03-13 11:39:19 +0800

    1.1.1

--- JYTools/demo/workerlog.py
+++ JYTools/demo/workerlog.py
@@ -6,7 +6,8 @@ from JYTools.JYWorker import ReadWorkerLog
 __author__ = '鹛桑够'
 
 rwl = ReadWorkerLog(log_dir="/tmp")
-exec_r, logs_list = rwl.read_task_log("Pipeline", "abcde", sub_key="", level="INFO")
+exec_r, logs_list = rwl.read_task_log("Plus", "6669", sub_key="1", level="INFO")
 for log in logs_list:
     print(log)
+    print(log[3])
 print(len(logs_list))
